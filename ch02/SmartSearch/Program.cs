d0f344764bedafb1f497188ae8b8a32c932755d7
frankabbruzzese <frankabbruzzese@gmail.com>
2019-03-21 17:34:19 +0100

    Added ch02 SmartSearch example

---
+++ ch02/SmartSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmartSearch
+{
+    class Program
+    {
+        static string[] fruits = new string[]{
+            "Apples", "Apricots", "Avocados",
+            "Bananas", "Boysenberries", "Blueberries", "Bing Cherry", "Blackberries",
+            "Cherries", "Cantaloupe", "Crab apples", "Clementine", "Cucumbers",
+            "Meloms", "Pears", "Grapes", "Strawberries", 
+        };
+        static void Main(string[] args)
+        {
+            var sd = new SmartDDictionary<string>(m => m, fruits);
+            bool finished = false;
+            while(!finished)
+            {
+                var search = Console.ReadLine();
+                Console.WriteLine();
+                foreach (var fruit in sd.Search(search, 5))
+                {
+                    Console.WriteLine(fruit);
+                }
+                Console.WriteLine("finished ?");
+                finished = (Console.ReadKey().KeyChar == 'y');
+            }
+        }
+    }
+}
---
+++ ch02/SmartSearch/readme.txt
@@ -0,0 +1,2 @@
+Create a dotnetcore consolle application and copy the SmartSearch folder content
+into the project folder. See "Designing a fast selection logic" subsection in chapter 2  
