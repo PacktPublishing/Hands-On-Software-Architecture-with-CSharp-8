da1a7b0f63a42afdb19241364244595b9bdf79f2
gabriell <gabriell@CE460>
2019-06-03 00:45:18 -0300

    Chapter 14

---
+++ ch14/CodeMetrics/SampleIDisposableClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeMetricsBadCode
+{
+    class SampleIDisposableClass : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
